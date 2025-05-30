c9e136903645f5b57f02bf1d959f113699a8a362
Олександр Мякушка <muakushka2020@gmail.com>
2025-05-30 11:29:43 +0300

    Update LambdaFunc.cs

--- LB8/Task3/LambdaFunc.cs
+++ LB8/Task3/LambdaFunc.cs
@@ -2,19 +2,15 @@
 
 public class LambdaFunc
 {
-    // Підрахунок чисел, кратних семи
     public static Func<int[], int> CountMultiplesOfSeven = 
         arr => arr.Count(n => n % 7 == 0 && n != 0);
 
-    // Підрахунок позитивних чисел
     public static Func<int[], int> CountPositiveNumbers = 
         arr => arr.Count(n => n > 0);
 
-    // Перевірка, чи є день програміста (256-й день року)
     public static Predicate<DateTime> IsProgrammerDay = 
         date => date.DayOfYear == 256;
 
-    // Перевірка на наявність заданого(их) слова(слів) у тексті
     public static Func<string, string[], bool> ContainsWords = 
         (text, words) => words.Any(word => text.Contains(word, StringComparison.OrdinalIgnoreCase));
 }
